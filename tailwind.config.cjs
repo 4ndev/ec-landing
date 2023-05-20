20d6f1fafab102faf3547a2db94838499cd44d9c
4ndev <4ndrei.93@gmail.com>
2023-05-20 02:41:42 +0200

    Added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Aeonik', ...defaultTheme.fontFamily.sans],
+		},
 		extend: {},
 	},
 	plugins: [],
